8bf9735fa11e4db77e8c1b074eb3f8256ed16af0
gcugli <gcugli@gmail.com>
2021-11-10 23:28:54 -0300

    pingbulk

--- etc/profile.d/gcugli.sh
+++ etc/profile.d/gcugli.sh
@@ -9,3 +9,17 @@ transfer () {
 		return 1
 	fi
 }
+
+###### pingbulk
+pingbulk () {
+	while IFS=$' ;:|\t\n\r' read ipaddress ignore
+	do
+		ping -c1 -W2 "$ipaddress" > /dev/null
+		if [ $? -eq 0 ]; then
+			echo "$ipaddress up"
+		else
+			echo "$ipaddress down"
+		fi
+	done
+}
+
